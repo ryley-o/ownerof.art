c35a0a4ed5639720b789cfa17fb64726fe9d4d3b
ryley-o <30364988+ryley-o@users.noreply.github.com>
2024-02-18 23:07:31 -0800

    natspec

--- src/IOwnerOf_Art.sol
+++ src/IOwnerOf_Art.sol
@@ -6,8 +6,15 @@ import {BytecodeStorageReader, BytecodeStorageWriter} from "lib/artblocks-contra
 import {IDelegateRegistry} from "lib/delegate-registry/src/IDelegateRegistry.sol";
 
 interface IOwnerOf_Art {
-    event MessagePosted(address indexed tokenAddress, uint256 indexed tokenId, address indexed sender, address bytecodeStorageAddress);
-    
+    /**
+     * Message posted event emitted when a message is posted about an ERC721 token by its owner (or delegate)
+     * @param tokenAddress Address of the ERC721 token contract being posted about
+     * @param tokenId ID of the token being posted about
+     * @param owner Address of the owner of the token sending the message
+     * @param bytecodeStorageAddress Address of the bytecode storage contract where the message is stored
+     */
+    event MessagePosted(address indexed tokenAddress, uint256 indexed tokenId, address indexed owner, address bytecodeStorageAddress);
+
     struct Message {
         address bytecodeStorageAddress;
         address sender;
@@ -21,14 +28,47 @@ interface IOwnerOf_Art {
         string message;
     }
 
+    /**
+     * @dev DELEGATE_REGISTRY is the address of the Delegate Registry contract
+     */
     function DELEGATE_REGISTRY() external view returns (address);
 
+    /**
+     * @notice Post a new message about an ERC721 token.
+     * The function will revert if the sender is not the owner of the token or a delegate of the owner on delegate.xyz v2.
+     * The message is stored in bytecode storage and the address of the storage contract is emitted in the MessagePosted event.
+     * The message may never be deleted or modified, but new messages may be posted.
+     * @dev Reentrant calls are prevented by the ReentrancyGuard modifier
+     * @param tokenAddress Address of the ERC721 token contract being posted about
+     * @param tokenId ID of the token being posted about
+     * @param message Message to be posted about the token
+     */
     function postMessage(address tokenAddress, uint256 tokenId, string memory message) external;
 
-    // @dev getMessages gas unbounded, use with caution, or use getMessageAtIndex for pagination
+    /**
+     * @notice Get all messages posted about an ERC721 token.
+     * @dev This function is gas unbounded and should be used with caution. For pagination, use getMessageAtIndex.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @return messagesView Array of MessageView structs containing the messages posted about the token
+     */
     function getMessages(address tokenAddress, uint256 tokenId) external view returns (MessageView[] memory);
 
+    /**
+     * @notice Get the number of messages posted about an ERC721 token.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @return count Number of messages posted about the token
+     */
     function getMessageCount(address tokenAddress, uint256 tokenId) external view returns (uint256);
 
+    /**
+     * @notice Get a message posted about an ERC721 token at a specific index.
+     * Reverts if the index is out of bounds.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @param index Index of the message to retrieve
+     * @return messageView MessageView struct containing the message posted about the token
+     */
     function getMessageAtIndex(address tokenAddress, uint256 tokenId, uint256 index) external view returns (MessageView memory);
 }
--- src/OwnerOf_Art.sol
+++ src/OwnerOf_Art.sol
@@ -19,6 +19,16 @@ contract OwnerOf_Art is IOwnerOf_Art, ReentrancyGuard {
 
     constructor() ReentrancyGuard() {}
 
+    /**
+     * @notice Post a new message about an ERC721 token.
+     * The function will revert if the sender is not the owner of the token or a delegate of the owner on delegate.xyz v2.
+     * The message is stored in bytecode storage and the address of the storage contract is emitted in the MessagePosted event.
+     * The message may never be deleted or modified, but new messages may be posted.
+     * @dev Reentrant calls are prevented by the ReentrancyGuard modifier
+     * @param tokenAddress Address of the ERC721 token contract being posted about
+     * @param tokenId ID of the token being posted about
+     * @param message Message to be posted about the token
+     */
     function postMessage(address tokenAddress, uint256 tokenId, string memory message) external nonReentrant {
         // EFFECTS
         // write message to bytecode storage, push to messages storage array
@@ -45,11 +55,16 @@ contract OwnerOf_Art is IOwnerOf_Art, ReentrancyGuard {
         }
 
         // EVENTS
-        emit MessagePosted(tokenAddress, tokenId, bytecodeStorageAddress, msg.sender);
-
+        emit MessagePosted({tokenAddress: tokenAddress, tokenId: tokenId, owner: tokenOwner, bytecodeStorageAddress: bytecodeStorageAddress);
     }
 
-    // @dev getMessages gas unbounded, use with caution, or use getMessageAtIndex for pagination
+    /**
+     * @notice Get all messages posted about an ERC721 token.
+     * @dev This function is gas unbounded and should be used with caution. For pagination, use getMessageAtIndex.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @return messagesView Array of MessageView structs containing the messages posted about the token
+     */
     function getMessages(address tokenAddress, uint256 tokenId) external view returns (MessageView[] memory) {
         Message[] storage messages = _messages[tokenAddress][tokenId];
         uint256 messagesLength = messages.length;
@@ -66,10 +81,24 @@ contract OwnerOf_Art is IOwnerOf_Art, ReentrancyGuard {
         return messagesView;
     }
 
+    /**
+     * @notice Get the number of messages posted about an ERC721 token.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @return count Number of messages posted about the token
+     */
     function getMessageCount(address tokenAddress, uint256 tokenId) external view returns (uint256) {
         return _messages[tokenAddress][tokenId].length;
     }
 
+    /**
+     * @notice Get a message posted about an ERC721 token at a specific index.
+     * Reverts if the index is out of bounds.
+     * @param tokenAddress Address of the ERC721 token contract posted about
+     * @param tokenId ID of the token posted about
+     * @param index Index of the message to retrieve
+     * @return messageView MessageView struct containing the message posted about the token
+     */
     function getMessageAtIndex(address tokenAddress, uint256 tokenId, uint256 index) external view returns (MessageView memory) {
         Message storage message = _messages[tokenAddress][tokenId][index];
         return MessageView({
